f2e4ece5af8f9d04c4ffca95d705622c562377bd
George <gsemmail@gmail.com>
2022-11-17 21:43:31 +0300

    footer is added

---
+++ src/components/CustomFooter.vue
@@ -0,0 +1,54 @@
+<template>
+	<div class="footer">
+		<div class="footer__info">
+			"Что творит Алис Вардо"
+			<p class="footer__info--copyright">
+				&copy;Jolly shop
+			</p>
+		</div>
+		<div class="footer__social">
+			<div class="footer__icon" />
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'CustomFooter'
+};
+</script>
+
+<style lang="scss" scoped>
+.footer {
+    background-color: #ebcf5d;
+    height: 5em;
+    padding: .5em;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    &__info {
+        color: #fff;
+        font-style: italic;
+    
+        &--copyright {
+            text-decoration: underline;
+            margin: 0;
+        }
+    }
+
+    &__social {
+        border-left: 3px solid #fff;
+        height: calc(100% - .5em);
+    }
+
+    &__icon {
+        background-image: url('/img/icons/footer_icon.png');
+        background-size: cover;
+        aspect-ratio: 1/1;
+        height: 100%;
+        margin-left: .5em;
+    }
+}
+</style>
+
